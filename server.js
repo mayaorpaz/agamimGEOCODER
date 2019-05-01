efab2fbb18afaa0e931abc1e04e60e725d322b77
Dan Halperin <dee.aye.en@gmail.com>
2019-05-01 14:02:18 +0300

    I have a lot of work to do

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 app.use(fileUpload());
 
 // ENTER YOUR GOOGLE API KEY HERE
-var myapi = "AIzaSyDXY_aO0xDGZX4BSOkw8w88wLpp0Q7HTIQ";
+var myapi = "AIzaSyDkG702RFFEEm08CP87sLK_amm-ru_eUVs";
 
 var options = {
   provider: "google",
@@ -31,6 +31,16 @@ app.listen(process.env.PORT || 3003, () => {
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/done", (req, res) => {
+  fs.readdir("./public/completed", function(err, items) {
+    if (items.length > 0) {
+      finalpath = "completed/" + items[items.length - 1];
+      console.log(finalpath);
+      res.render("done.ejs", { finalpath });
+    }
+  });
 });
 
 app.get("/select", (req, res) => {
@@ -101,7 +111,7 @@ app.get("/select", (req, res) => {
           "AY",
           "AZ"
         ];
-        console.log(columns);
+        //console.log(columns);
         res.render("select.ejs", { columns, columnCount, filename, alphabet });
       });
     }
@@ -139,130 +149,71 @@ app.post("/geocode", function(req, res) {
         filename = worksheet.name;
         columns = row.values;
         columnCount = worksheet.columnCount;
-        console.log(addressColumn.values);
+        addressList = [];
+        //console.log(typeof addressColumn.values);
+        //console.log(addressColumn.values[3].result);
+        if (addressColumn.values[3].result != undefined) {
+          addressColumn.eachCell(function(cell, rowNumber) {
+            addressList.push(cell.result);
+            //console.log("hello");
+          });
+        } else {
+          addressList = addressColumn.values;
+          console.log("hello2");
+        }
+        console.log(addressList.slice(0, 10));
+
+        var tempx = [];
+        var tempy = [];
+        async.each(
+          addressList,
+          function(addr, callback) {
+            //console.log(addr);
+            setTimeout(function() {
+              //console.log("Current: " + addr);
+              if (addr != undefined) {
+                geocoder.geocode(addr, function(err, geocoded) {
+                  if (err) {
+                    callback(err);
+                    return;
+                  }
+                  if (geocoded) {
+                    if (geocoded != undefined) {
+                      tempx.push(geocoded[0].latitude);
+                      tempy.push(geocoded[0].longitude);
+                    } else {
+                      tempx.push("undefined");
+                      tempy.push("undefined");
+                    }
+                    callback();
+                  }
+                });
+              }
+            }, 50);
+          },
+          function(err) {
+            if (err) {
+              console.log(err);
+            }
+
+            console.log(tempx.length);
+            console.log(tempy.length);
+            worksheet.getColumn("X").values = tempx;
+            worksheet.getColumn("Y").values = tempy;
+            fs.readdir("./public/completed", function(err, items) {
+              placeholder = 0;
+              if (items != undefined) {
+                placeholder = items.length;
+              }
+              mypath2 = "./public/completed/" + placeholder + ".xlsx";
+              workbook.xlsx.writeFile(mypath2).then(function() {
+                console.log(mypath2 + " -- file is written.");
+                res.redirect("/done");
+              });
+            });
+          }
+        );
       });
     }
   });
-  console.log(req.body);
-  res.redirect("/select");
-});
-
-// HERE I AM READING THE 6TH COLUMN IN THE WORKSHEET WHICH IS HOLDING THE ADDRESSES,
-// LOOPING THROUGH THEM, AND GEOCODING THEM. THE RESULTING X AND Y ARE BEING SAVED IN
-// TEMPX AND TEMPY ARRAYS. I THEN INSERT THEM BACK INTO THE SHEET IN NEW COLUMNS AND SAVE.
-
-fs.readdir("./public/files", function(err, items) {
-  if (items.length > 0) {
-    mypath = "./public/files/" + items[items.length - 1];
-    console.log(mypath);
-    var workbook = new Excel.Workbook();
-
-    workbook.xlsx.readFile(mypath).then(function() {
-      var worksheet = workbook.getWorksheet(1);
-      var row = worksheet.getRow(1);
-      var addressColumn = worksheet.getColumn(6);
-
-      //console.log(row.values);
-      //console.log(worksheet.columnCount);
-
-      var addressList = [];
-      addressColumn.eachCell(function(cell, rowNumber) {
-        addressList.push(cell.value.result);
-      });
-
-      //console.log(addressList);
-
-      // FAILED ATTEMPT AT PROMISE ASYNC AWAIT
-
-      /*function myGeocode(myAddress) {
-        return new Promise(function(resolve, reject) {
-          let myobj;
-          geocoder.geocode(myAddress, function(err, res) {
-            if (res != undefined) {
-              resolve(res[0].latitude);
-            }
-          });
-        });
-      }
-
-      Promise.all(addressList.map(myGeocode))
-        .then(function(geocodedList) {
-          console.log(geocodedList);
-        })
-        .catch(function(error) {
-          console.error(error);
-        });*/
-
-      var tempx = [];
-      var tempy = [];
-      async.each(addressList, function(addr, callback){
-        setTimeout(function() {
-        console.log("Current: " + addr)
-          if(addr != undefined){
-            geocoder.geocode(addr, function(err, geocoded){
-              if(err){
-                callback(err);
-                return
-              }
-              if(geocoded) {
-                if(geocoded != undefined){
-                  tempx.push(geocoded[0].latitude)
-                  tempy.push(geocoded[0].longitude)
-                } else {
-                  tempx.push("undefined")
-                  tempy.push("undefined")
-                }
-                callback();
-              }
-            });
-          }
-        }, 10)
-      }, function(err){
-        if(err){
-          console.log(err)
-        } else {
-          //console.log(tempx)
-          //console.log(tempy)
-        }
-
-        console.log(tempx.length)
-        console.log(tempy.length)
-        worksheet.getColumn("X").values = tempx;
-        worksheet.getColumn("Y").values = tempy;
-        workbook.xlsx.writeFile("./public/files/0test.xlsx").then(function() {
-          console.log("xlsx file is written.");
-        });
-      })
-      /*for (var j = 0; j < addressList.length; j++) {
-        geocoder.geocode(addressList[j], function(err, res) {
-          //console.log(res);
-          if (res != undefined) {
-            console.log(res[0].latitude);
-            tempx.push(res[0].latitude);
-            tempy.push(res[0].longitude);
-          } else {
-            tempx.push("undefined");
-            tempy.push("undefined");
-          }
-        });
-      }*/
-
-      // INSERTING TEMPX & TEMPY ARRAY INTO 'X' & 'Y' COLUMNS
-
-      //worksheet.getColumn("x").values = tempx;
-      //worksheet.getColumn("y").values = tempy;
-
-      // WRITING TO NEW FILE
-
-      /*workbook.xlsx.writeFile("./public/files/0test.xlsx").then(function() {
-        console.log("xlsx file is written.");
-      });*/
-
-      // BATCH GEOCODING ISN'T ALLOWED WITH GOOGLE API
-
-      /*geocoder.batchGeocode(addressList, function(err, res) {
-        console.log(res);
-      });*/
-    });
-  }
 });
